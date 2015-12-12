86de62ae4bb9de11fb50f2a27f94fa1cb84cb0a8
Garen Torikian <gjtorikian@gmail.com>
2015-12-11 22:12:45 -0800

    Allow attributes to be modified at compile time
    Without this, there’s no way to tell what an item rep is while
    resolving conrefs.

--- lib/nanoc-conref-fs/conref-filter.rb
+++ lib/nanoc-conref-fs/conref-filter.rb
@@ -1,7 +1,27 @@
+module Nanoc
+  class ItemView
+    # allows apply_attributes to create new item attributes
+    def []=(key, value)
+      unwrap.attributes[key] = value
+    end
+  end
+end
+
+module Nanoc::HashExtensions
+  alias_method :original_freeze, :__nanoc_freeze_recursively
+
+  # prevents the item's frozen attributes from remaining frozen
+  def __nanoc_freeze_recursively
+    return if caller.first =~ %r{base/entities/document.rb}
+    original_freeze
+  end
+end
+
 class ConrefFSFilter < Nanoc::Filter
   identifier :'conref-fs-filter'
 
   def run(content, _)
+    ConrefFS.apply_attributes(@config, item)
     NanocConrefFS::Conrefifier.liquify(@item[:filename], content, @config.unwrap)
   end
 end
