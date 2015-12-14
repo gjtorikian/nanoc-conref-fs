9dd65c0a197ed763c3ec834216a095091d7d4750
Garen Torikian <gjtorikian@gmail.com>
2015-12-14 12:09:31 -0800

    Introduce simpler ways to construct rep identities

--- lib/nanoc-conref-fs/conref-fs.rb
+++ lib/nanoc-conref-fs/conref-fs.rb
@@ -1,4 +1,5 @@
 require_relative 'conrefifier'
+require 'active_support/core_ext/string'
 
 class ConrefFS < Nanoc::DataSource
   include Nanoc::DataSources::Filesystem
@@ -10,10 +11,11 @@ class ConrefFS < Nanoc::DataSource
   # Before iterating over the file objects, this method loads the data folder
   # and applies it to an ivar for later usage.
   def up
-    return unless NanocConrefFS::Variables.data_files.nil?
     data_files = NanocConrefFS::Datafiles.collect_data(data_dir_name)
     NanocConrefFS::Variables.data_files = data_files
     NanocConrefFS::Variables.variables = {}
+    reps = @config[:reps] || [:default]
+    reps.each { |rep| ConrefFS.load_data_folder(@site_config, rep) }
   end
 
   def data_dir_name
@@ -52,15 +54,14 @@ class ConrefFS < Nanoc::DataSource
     end
   end
 
-  # This file reads each piece of content as it comes in. It also applies the conref variables
-  # (demarcated by Liquid's {{ }} tags) using both the data/ folder and any variables defined
-  # within the nanoc.yaml config file
+  # This file reads each piece of content as it comes in. It also catches and fixes
+  # an error where YAML frontmatter cannot start with a curly brace
+  # (eg. intro: '{{ ... }}')
   def read(filename)
     content = super
-    content = content.gsub(/\A---\s*\n(.*?\n?)^---\s*$\n?/m) do |frontmatter|
-      frontmatter.gsub(/:\s*(\{\{.+)/) do |_|
-        curly_match = Regexp.last_match[1]
-        ": '#{curly_match}'"
+    content.gsub(/^([^:]+): (\{\{.+)/, '\1: \'\2\'')
+  end
+
       end
     end
     content
